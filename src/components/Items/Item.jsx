3a89772b1ae1e8bbd4e11f4331e908535b1784c8
Nastya <skreka.nastya@gmail.com>
2023-09-21 21:08:06 +0300

    fix styles

--- src/components/Items/Item.jsx
+++ src/components/Items/Item.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import styles from '../../styles/FormItem.module.css'
+import styles from '../../styles/Item.module.css'
 
 const ListItem = ({ onDelete, item, onSelect, isActive }) => {
   return (
     <div className={styles.card}>
       <div className={styles.container}>
-        <p>{item.name}</p>
-        <p>Comments: {item.comments.length}</p>
+        <p className={styles.text}>{item.name}</p>
+        <p className={styles.text}>Comments: {item.comments.length}</p>
         <div className={styles.actions}>
           <button onClick={onDelete} className={styles.delete}>
             Delete{' '}
